8bbc6a748ded381f1dcbb96241b21606f2eecde3
Edoardo Nero <127231073+mrblack1998@users.noreply.github.com>
2024-08-29 10:18:25 +0200

    Delete PHP_Verifica_Biglietto directory

--- PHP_Verifica_Biglietto/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$conn = new mysqli('localhost', 'root', '', 'gestionale_swe');
-if ($conn->connect_error) {
-    die('Errore di connessione (' . $conn->connect_errno . ') ' . $conn->connect_error);
-}
-$conn->set_charset('utf8');
-?>
