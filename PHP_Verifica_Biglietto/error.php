a8d7368cf072e3f20853993e1d3f0b88133eb7c7
Edoardo Nero <127231073+mrblack1998@users.noreply.github.com>
2024-08-29 10:21:53 +0200

    Applicativo Web per verifica biglietto
    Realizzato in PHP e JS (implementa la libreria Quagga.js), permette la verifica dei biglietti tramite l'utilizzo del browser. Consentendo ai vari IC di utilizzare lo smartphone anziché il PC.

---
+++ PHP_Verifica_Biglietto/error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Progetto SWE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/flexbox.css" rel="stylesheet">
+    </head>
+
+    <body style="height: 100vh; width: 100%; background-color: #b61111;" class="center_column" onclick="location.href='index.php'">
+        <h1>Biglietto Non Valido!</h1>
+    </body>
+</html>
